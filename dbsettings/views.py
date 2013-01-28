9b0fe73312472a4682a2cefb264abc76449de2da
Konrad Wojas <github@m.wojas.nl>
2013-01-28 20:19:50 +0800

    If the user is a superuser, we do not require staff; Added logging; If nothing was changed, show a message saying this

--- dbsettings/views.py
+++ dbsettings/views.py
@@ -1,15 +1,25 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.text import capfirst
 from django.utils.translation import ugettext_lazy as _
 from django.contrib import messages
 
 from dbsettings import loading, forms
 
+import logging
+log = logging.getLogger(__name__)
 
-@staff_member_required
+def staff_or_superuser(user):
+    """
+    :type user: django.contrib.auth.models.User
+    """
+    if user and user.is_active and (user.is_staff or user.is_superuser):
+        return True
+    return False
+
+@user_passes_test(staff_or_superuser)
 def app_settings(request, app_label, template='dbsettings/app_settings.html'):
     # Determine what set of settings this editor is used for
     if app_label is None:
@@ -28,13 +38,16 @@ def app_settings(request, app_label, template='dbsettings/app_settings.html'):
         if form.is_valid():
             form.full_clean()
 
+            n_changed = 0
             for name, value in form.cleaned_data.items():
                 key = forms.RE_FIELD_NAME.match(name).groups()
                 setting = loading.get_setting(*key)
                 try:
                     storage = loading.get_setting_storage(*key)
                     current_value = setting.to_python(storage.value)
-                except:
+                except Exception, e:
+                    log.info("An exception occurred trying to convert %s, "
+                        "with value %s, to python.", name, value, exc_info=True)
                     current_value = None
 
                 if current_value != setting.to_python(value):
@@ -49,6 +62,11 @@ def app_settings(request, app_label, template='dbsettings/app_settings.html'):
                     update_msg = (_(u'Updated %(desc)s on %(location)s') %
                                   {'desc': unicode(setting.description), 'location': location})
                     messages.add_message(request, messages.INFO, update_msg)
+                    n_changed += 1
+
+            if not n_changed:
+                no_update_msg = (_(u'Nothing was changed'))
+                messages.add_message(request, messages.INFO, no_update_msg)
 
             return HttpResponseRedirect(request.path)
     else:
@@ -64,4 +82,4 @@ def app_settings(request, app_label, template='dbsettings/app_settings.html'):
 # Site-wide setting editor is identical, but without an app_label
 def site_settings(request):
     return app_settings(request, app_label=None, template='dbsettings/site_settings.html')
-# staff_member_required is implied, since it calls app_settings
+# security is checked on app_settings
